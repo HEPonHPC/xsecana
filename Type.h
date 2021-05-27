9af286f3f2de3907a6e37f84ca8dde30b6e5202c
Derek Doyle <derek.doyle@colostate.edu>
2021-05-27 21:09:38 +0000

    Type checking facilities
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/novaart/pkgs.svn/branches/feature_xsec_ana_br/XSecAna@48150 42286b90-2e16-4323-8552-92d8e493c5d4

---
+++ Type.h
@@ -0,0 +1,12 @@
+#include <type_traits>
+#include "XSecAna/Hist.h"
+
+namespace xsec {
+  namespace type {
+    template<class Scalar, int Cols> std::true_type _is_hist(const Hist<Scalar, Cols>*);
+    std::false_type _is_hist(...);
+
+    template<class T>
+    struct IsHist : decltype(_is_hist(std::declval<T*>())) {};
+  }
+}
