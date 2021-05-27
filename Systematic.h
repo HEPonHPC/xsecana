29839659f1eb854dc1db93144a9f3ffe1cfc2d76
Derek Doyle <derek.doyle@colostate.edu>
2021-05-27 21:11:00 +0000

    Multiverse uncertainty calculation. Add to systematics test
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/novaart/pkgs.svn/branches/feature_xsec_ana_br/XSecAna@48152 42286b90-2e16-4323-8552-92d8e493c5d4

--- Systematic.h
+++ Systematic.h
@@ -3,12 +3,17 @@
 #include "TDirectory.h"
 
 #include <functional>
+#include "XSecAna/Type.h"
 namespace xsec {
 
   //-------------------------------------------------------
   template<class T>
   class Systematic {
   public:
+    Systematic(std::string name) 
+      : fName(name)
+    {}
+
     Systematic(std::string name,
 	       T up,
 	       T dw)
@@ -47,13 +52,13 @@ namespace xsec {
   class MultiverseSystematic : public Systematic<T> {
   public:
     MultiverseSystematic(std::string name,
-			 std::vector<T*>  universes)
+			 std::vector<T>  universes)
       : Systematic<T>(name),
 	fUniverses(universes)
     {}
       
     template<class F, class... Args>
-    Systematic<std::invoke_result_t<F, T, Args...> > Invoke(F f, Args... args) const;
+    MultiverseSystematic<std::invoke_result_t<F, T, Args...> > Invoke(F f, Args... args) const;
 
     void SaveTo(TDirectory * dir, std::string name) const; 
     
@@ -63,8 +68,12 @@ namespace xsec {
     
     static std::unique_ptr<MultiverseSystematic> LoadFrom(TDirectory * dir, std::string name); 
 
+    auto NSigmaShift(double nsigma,
+		     const T & nominal) const;
   private:
-    double BinSigma(std::vector<double> values, double nsigma, double pivot);
+
+    template<class Scalar>
+    Scalar BinSigma(const double & nsigma, std::vector<Scalar> & universes, const Scalar & nominal) const;
     std::vector<T> fUniverses;
 
   };
@@ -90,12 +99,12 @@ namespace xsec {
   ////////////////////////////////////////////////////////////
   template<class T>
   template<class F, class... Args>
-  Systematic<std::invoke_result_t<F, T, Args...> > 
+  MultiverseSystematic<std::invoke_result_t<F, T, Args...> > 
   MultiverseSystematic<T>::Invoke(F f, Args... args) const
   {
     std::vector<std::invoke_result_t<F, T, Args...> > universes(this->fUniverses.size());
     for(auto i = 0u; i < fUniverses.size(); i++) {
-      universes[i] = std::invoke(&f, *this->fUniverses[i], args...);
+      universes[i] = std::invoke(f, this->fUniverses[i], args...);
     }
     return MultiverseSystematic<std::invoke_result_t<F, T, Args...> >(this->fName,
 								      universes);
@@ -113,13 +122,14 @@ namespace xsec {
 
     TObjString("MultiverseSystematic").Write("type");
     TObjString(this->fName.c_str()).Write("fName");
-
+    TObjString(std::to_string(this->fUniverses.size()).c_str()).Write("NUniverses");
+    
+    
     auto mv_dir = dir->mkdir("fUniverses");
     for(auto i = 0u; i < fUniverses.size(); i++) {
-      fUniverses[i]->SaveTo(mv_dir, std::to_string(i));
+      fUniverses[i].SaveTo(mv_dir, std::to_string(i));
     }
 
-    delete dir;
     tmp->cd();
   }
 
@@ -154,10 +164,11 @@ namespace xsec {
     delete ptag;
     
     std::string name = ((TObjString*) dir->Get("fName"))->GetString().Data();
-    std::vector<T*> universes;
+    int nuniverses = std::atoi(((TObjString*) dir->Get("NUniverses"))->GetString().Data());
+    std::vector<T> universes(nuniverses);
     auto mv_dir = dir->GetDirectory("fUniverses");
-    for(auto universe : *mv_dir->GetListOfKeys()) {
-      universes.push_back(T::LoadFrom(dir, universe->GetName()).release());
+    for(auto imv = 0; imv < nuniverses; imv++) {
+      universes[imv] = *T::LoadFrom(mv_dir, std::to_string(imv)).release();
     }
     return std::make_unique<MultiverseSystematic<T> >(name, universes);
   }
@@ -185,6 +196,88 @@ namespace xsec {
       std::unique_ptr<T> up = T::LoadFrom(dir, "fUp");
       return std::make_unique<Systematic<T> >(name, *up);
     }
-
   }
+
+  ////////////////////////////////////////////////////////////
+  /*
+  template<class T>
+  template<template<class, int> Hist,
+	   class Scalar,
+	   int Cols>  
+  Hist<Scalar, Cols>
+  MultiverseSystematic<T>::
+  NSigmaShift(double sigma,
+	      const std::vector<Hist<Scalar, Cols> > & universes,
+	      const Hist<Scalar, Cols> & nominal)
+  {
+    Eigen::Array<Scalar, 1,  Cols> shift(nominal.NBins());
+
+    for(auto ibin = 0u; ibin < nominal.NBins(); ibin++) {
+      std::vector<Scalar> vals;
+      for(auto iuniv = 0u; iuniv < universes.size(); iuniv++) {
+	vals.push_back(universes.Contents()(ibin));
+      }
+      shift(ibin) = BinSigma(sigma, vals, nominal.Contents(ibin));
+    }
+
+    return shift;
+  }
+  */
+  ////////////////////////////////////////////////////////////
+  template<class T>
+  template<class Scalar>
+  Scalar
+  MultiverseSystematic<T>::
+  BinSigma(const double & nsigma,
+	   std::vector<Scalar> & universes,
+	   const Scalar & nominal) const
+  {
+    int pivotbin = 0;
+    double pivotbincenter = 0;
+    std::sort(universes.begin(), universes.end());
+    for(auto i = 0u; i < universes.size() - 1; i++) {
+      if(nominal >= universes[i] && nominal < universes[i+1]) {
+	pivotbin = i;
+	break;
+      }
+    }
+    pivotbincenter = pivotbin+0.5;
+    double count_fraction = std::erf(nsigma / std::sqrt(2));
+
+    int nsideevents = 0;
+    int lastbinindex = (int) universes.size() - 1;
+    if(nsigma >= 0) nsideevents = lastbinindex - pivotbin;
+    else nsideevents = pivotbin;
+    int boundIdx = pivotbincenter + count_fraction*(double)nsideevents;
+
+    int index = 0;
+    if(nsigma >= 0) index = std::min(boundIdx, (int)universes.size() - 1);
+    else index = std::max(boundIdx, 0);
+    return universes.at(index);
+  }
+
+  ////////////////////////////////////////////////////////////
+  template<class T>
+  auto 
+  MultiverseSystematic<T>::
+  NSigmaShift(double nsigma, 
+	      const T & nominal) const
+  {
+    if constexpr(type::IsHist<T>()) {
+	T shift = nominal;
+
+	for(auto ibin = 0u; ibin < nominal.NBins(); ibin++) {
+	  std::vector<decltype(std::declval<T>().GetBinContent(1))> vals;
+	  for(auto iuniv = 0u; iuniv < fUniverses.size(); iuniv++) {
+	    vals.push_back(fUniverses[iuniv].GetBinContent(ibin));
+	  }
+	  shift.SetBinContent(ibin, BinSigma(nsigma, vals, nominal.GetBinContent(ibin)));
+	}
+	return shift;
+      }
+    else {
+      return this->Invoke(&T::ToHist).NSigmaShift(nsigma, nominal.ToHist());
+    }
+  }
+
 }
--- test/test_systematic.cc
+++ test/test_systematic.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 #include "XSecAna/Hist.h"
 #include "XSecAna/Systematic.h"
@@ -7,6 +8,25 @@
 #include "TFile.h"
 
 using namespace xsec;
+
+template<class Scalar, 
+	 int Cols>
+struct Ratio {
+  Ratio(Hist<Scalar, Cols> num,
+	Hist<Scalar, Cols> den)
+    : numerator(num), denominator(den)
+  {}
+
+  Hist<Scalar, Cols> numerator;
+  Hist<Scalar, Cols> denominator;
+  
+  Hist<Scalar, Cols> ToHist() const
+  {
+    return numerator / denominator;
+  }
+  
+};
+
 #define TEST_ARRAY(test_name, arr1, arr2, precision)			\
   test = (arr1 - arr2).isZero(precision);				\
   if(!test || verbose) {						\
@@ -32,8 +52,23 @@ using namespace xsec;
     pass = false;							\
   }									
 
+#define TEST_MULTIVERSE(test_name, mv1, mv2)	\
+  test = true;								\
+  for(auto imv = 0u; imv < (mv1).GetUniverses().size(); imv++) {		\
+    test &= (mv1).GetUniverses()[imv] == (mv2).GetUniverses()[imv];	\
+  }									\
+  if(!test || verbose) {						\
+    std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << (test? ": PASSED" : ": FAILED") << std::endl; \
+    for(auto imv = 0u; imv < (mv1).GetUniverses().size(); imv++) {	\
+      std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "[" << imv << "]\t" << (mv1).GetUniverses()[imv].Contents() << std::endl; \
+      std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "[" << imv << "]\t" << (mv2).GetUniverses()[imv].Contents() << std::endl; \
+    }									\
+    pass = false;							\
+  }									\
+
+
 template<typename Scalar, int Cols>
-bool run_tests(bool verbose)
+bool run_tests(bool verbose, std::string dir)
 {
   bool pass = true;
   bool test;
@@ -76,14 +111,18 @@ bool run_tests(bool verbose)
     syst_diff_1.Invoke(static_cast<histtype(histtype::*)(const histtype&) const>(&histtype::operator/), nominal);
   TEST_SYSTEMATIC("one sided division", syst_div_1, (up - nominal) / nominal, (up - nominal) / nominal);
 
-  TFile * output = new TFile("test_systematic.root", "recreate");
-  syst_2.SaveTo(output, "syst_2");
-  syst_1.SaveTo(output, "syst_1");
+  TFile * output = new TFile("test_systematic.root", "update");
+  TDirectory * to = output->mkdir(dir.c_str());
+  syst_2.SaveTo(to, "syst_2");
+  syst_1.SaveTo(to, "syst_1");
   output->Close();
+  delete output;
 
   TFile * input = TFile::Open("test_systematic.root");
-  auto loaded_2 = Systematic<histtype>::LoadFrom(input, "syst_2");
-  auto loaded_1 = Systematic<histtype>::LoadFrom(input, "syst_1");
+  auto loaded_2 = Systematic<histtype>::LoadFrom(input->GetDirectory(dir.c_str()), "syst_2");
+  auto loaded_1 = Systematic<histtype>::LoadFrom(input->GetDirectory(dir.c_str()), "syst_1");
+  input->Close();
+  delete input;
 
   TEST_SYSTEMATIC("load 2-sided", (*loaded_2), up, down);
   TEST_SYSTEMATIC("load 1-sided", (*loaded_1), up, up  );
@@ -91,6 +130,80 @@ bool run_tests(bool verbose)
   return pass;
 }
 
+template<typename Scalar, int Cols>
+bool run_tests_mv(bool verbose, std::string dir)
+{
+  typedef Hist<Scalar, Cols> histtype;
+
+  bool pass = true;
+  bool test;
+  auto bins = Eigen::Array<Scalar, 1, EdgesSize(Cols)>::LinSpaced(11, 0, 10);
+  
+  auto vnominal = Eigen::Array<Scalar, 1, Cols>::LinSpaced(10, -5, 5);
+  Hist<Scalar, Cols> nominal(vnominal, bins);
+  int nuniverses = 50;
+  Scalar maxy =  1;
+  Scalar miny = -1;
+  std::vector<Hist<Scalar, Cols> > universes(nuniverses);
+  for(auto i = 0; i < nuniverses; i++) {
+    universes[i] = nominal + (-1 + (maxy - miny) / (nuniverses-1) * i);
+  }
+  
+  MultiverseSystematic syst("test_mv", universes);
+
+  auto plus_1sigma = syst.NSigmaShift(1, nominal);
+  auto minus_1sigma = syst.NSigmaShift(-1, nominal);
+
+  // MultiverseSystematic::NSigmaShift returns a 
+  // histogram representing closest universes to 1 sigma away from nominal
+  // this is done bin by bin, so the returned histogram holds values that are found
+  // in the multiverse
+  // With a constant shift up and down, the resulting histogram will be the exact same 
+  // histogram from one of the universes.
+  // For a maximum spread of +/- 1, the shift that gets reconstructed
+  // represents the universe corresponding to the 15th and 85th percentile index, for -/+ 1sigma,
+  // respectively, in a sorted array of these universes.
+  // This 
+  int p1_idx = (0.5 - std::erf(1 / std::sqrt(2)) / 2.0) * (nuniverses-1) + 1;
+  TEST_ARRAY("minus 1 sigma", minus_1sigma.Contents(), universes[p1_idx].Contents(), 0);
+
+  // Test the ability of MultiverseSystematic<T> to call T::ToHist
+  // No need to get too fancy here. If it compiles assume it passes
+  std::vector<Ratio<Scalar, Cols> >vratio_mv;
+  for(auto i = 0u; i < syst.GetUniverses().size(); i++) {
+    vratio_mv.push_back(Ratio(syst.GetUniverses()[i], nominal));
+  }
+  
+  MultiverseSystematic ratio_mv("ratio_mv", vratio_mv);
+  MultiverseSystematic ratio_mv_hists = ratio_mv.Invoke(&Ratio<Scalar, Cols>::ToHist);
+
+  auto ratio_plus_1sigma = ratio_mv.NSigmaShift(1, Ratio(nominal, nominal));
+  auto ratio_minus_1sigma = ratio_mv.NSigmaShift(-11, Ratio(nominal, nominal));
+
+  
+  // save everything for later inspection
+  TFile * output = new TFile("test_systematic.root", "update");
+  syst.SaveTo(output->mkdir(dir.c_str()), "test_mv");
+  ratio_mv_hists.SaveTo(output->GetDirectory(dir.c_str()), "ratio_mv");
+  nominal.SaveTo(output->GetDirectory(dir.c_str()), "nominal");
+  plus_1sigma.SaveTo(output->GetDirectory(dir.c_str()), "plus_1sigma");
+  minus_1sigma.SaveTo(output->GetDirectory(dir.c_str()), "minus_1sigma");
+  ratio_plus_1sigma.SaveTo(output->GetDirectory(dir.c_str()),  "ratio_plus_1sigma");
+  ratio_minus_1sigma.SaveTo(output->GetDirectory(dir.c_str()), "ratio_minus_1sigma");
+
+
+  output->Close();
+  delete output;
+
+  // serialization closure
+  TFile * input = TFile::Open("test_systematic.root");
+  auto loaded = MultiverseSystematic<histtype>::LoadFrom(input->GetDirectory(dir.c_str()), "test_mv");
+  input->Close();
+  delete input;
+  TEST_MULTIVERSE("loadfrom/saveto", syst, *loaded);
+
+  return pass;
+}
 
 int main(int argc, char ** argv)
 {
@@ -98,9 +211,17 @@ int main(int argc, char ** argv)
   if(argc > 1 && std::strcmp(argv[1], "-v") == 0)  verbose = true;
 
   bool pass = true;
-  pass &= run_tests<double, 10>(verbose);
-  pass &= run_tests<double, Eigen::Dynamic>(verbose);
-  pass &= run_tests<float, 10>(verbose);
-  pass &= run_tests<float, Eigen::Dynamic>(verbose);
+
+  std::remove("test_systematic.root");
+
+  pass &= run_tests<double, 10>(verbose, "double_10");
+  pass &= run_tests<double, Eigen::Dynamic>(verbose, "double_dynamic");
+  pass &= run_tests<float, 10>(verbose, "float_10");
+  pass &= run_tests<float, Eigen::Dynamic>(verbose, "float_dynamic");
+
+  pass &= run_tests_mv<double, 10>(verbose, "mv_double_10");
+  pass &= run_tests_mv<double, Eigen::Dynamic>(verbose, "mv_double_dynamic");
+  pass &= run_tests_mv<float, 10>(verbose, "mv_float_10");
+  pass &= run_tests_mv<float, Eigen::Dynamic>(verbose, "mv_float_dynamic");
   if(pass) std::cout << "Success!" << std::endl;
 }
