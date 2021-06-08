53f4cecd4ccb0ff16e5c192827e85495e3c29cc4
Derek Doyle <derek.doyle@colostate.edu>
2021-06-08 22:38:03 +0000

    Updated test of Systematic
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/novaart/pkgs.svn/branches/feature_xsec_ana_br/XSecAna@48238 42286b90-2e16-4323-8552-92d8e493c5d4

--- test/test_systematic.cc
+++ test/test_systematic.cc
@@ -12,6 +12,8 @@ using namespace xsec;
 template<class Scalar, 
 	 int Cols>
 struct Ratio {
+  Ratio() {}
+
   Ratio(Hist<Scalar, Cols> num,
 	Hist<Scalar, Cols> den)
     : numerator(num), denominator(den)
@@ -37,31 +39,31 @@ struct Ratio {
   }									
 
 #define TEST_SYSTEMATIC(test_name, syst, up, down)			\
-  test = *syst.GetShifts().first == (up);				\
+  test = syst.Up() == (up);						\
   if(!test || verbose) {						\
     std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << (test? ": PASSED" : ": FAILED") << std::endl; \
-    std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "\t" << syst.GetShifts().first->Contents() << std::endl; \
+    std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "\t" << syst.Up().Contents() << std::endl; \
     std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "\t" << (up).Contents() << std::endl; \
     pass = false;							\
   }									\
-  test = *syst.GetShifts().second == (down);				\
+  test = syst.Down() == (down);					\
   if(!test || verbose) {				\
     std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << (test? ": PASSED" : ": FAILED") << std::endl; \
-    std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "\t" << syst.GetShifts().second->Contents() << std::endl; \
+    std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "\t" << syst.Down().Contents() << std::endl; \
     std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "\t" << (down).Contents() << std::endl; \
     pass = false;							\
   }									
 
 #define TEST_MULTIVERSE(test_name, mv1, mv2)	\
   test = true;								\
-  for(auto imv = 0u; imv < (mv1).GetUniverses().size(); imv++) {		\
-    test &= (mv1).GetUniverses()[imv] == (mv2).GetUniverses()[imv];	\
+  for(auto imv = 0u; imv < (mv1).GetShifts().size(); imv++) {		\
+    test &= (mv1).GetShifts()[imv] == (mv2).GetShifts()[imv];	\
   }									\
   if(!test || verbose) {						\
     std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << (test? ": PASSED" : ": FAILED") << std::endl; \
-    for(auto imv = 0u; imv < (mv1).GetUniverses().size(); imv++) {	\
-      std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "[" << imv << "]\t" << (mv1).GetUniverses()[imv].Contents() << std::endl; \
-      std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "[" << imv << "]\t" << (mv2).GetUniverses()[imv].Contents() << std::endl; \
+    for(auto imv = 0u; imv < (mv1).GetShifts().size(); imv++) {	\
+      std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "[" << imv << "]\t" << (mv1).GetShifts()[imv].Contents() << std::endl; \
+      std::cerr << __PRETTY_FUNCTION__ << "\t" << test_name << "[" << imv << "]\t" << (mv2).GetShifts()[imv].Contents() << std::endl; \
     }									\
     pass = false;							\
   }									\
@@ -88,13 +90,11 @@ bool run_tests(bool verbose, std::string dir)
 
   // two sided systematic subtraction via invoke
   typedef Hist<Scalar, Cols> histtype;
-  Systematic<Hist<Scalar, Cols> > syst_diff_2 = 
-    syst_2.Invoke(static_cast<histtype(histtype::*)(const histtype&) const>(&histtype::operator-), nominal);
+  auto syst_diff_2 = syst_2.Invoke(static_cast<histtype(histtype::*)(const histtype&) const>(&histtype::operator-), nominal);
   TEST_SYSTEMATIC("two sided subtraction", syst_diff_2, up - nominal, down - nominal);
 
   // two sided systematic division via invoke
-  Systematic<Hist<Scalar, Cols> > syst_div_2 = 
-    syst_diff_2.Invoke(static_cast<histtype(histtype::*)(const histtype&) const>(&histtype::operator/), nominal);
+  auto syst_div_2 = syst_diff_2.Invoke(static_cast<histtype(histtype::*)(const histtype&) const>(&histtype::operator/), nominal);
   TEST_SYSTEMATIC("two sided division", syst_div_2, (up - nominal) / nominal, (down - nominal) / nominal);
   
   // one sided systematic construction
@@ -102,13 +102,11 @@ bool run_tests(bool verbose, std::string dir)
   TEST_SYSTEMATIC("construction", syst_1, up, up);
 
   // one sided systematic subtraction via invoke
-  Systematic<Hist<Scalar, Cols> > syst_diff_1 = 
-    syst_1.Invoke(static_cast<histtype(histtype::*)(const histtype&) const>(&histtype::operator-), nominal);
+  auto syst_diff_1 = syst_1.Invoke(static_cast<histtype(histtype::*)(const histtype&) const>(&histtype::operator-), nominal);
   TEST_SYSTEMATIC("one sided subtraction", syst_diff_1, up - nominal, up - nominal);
 
   // one sided systematic division via invoke
-  Systematic<Hist<Scalar, Cols> > syst_div_1 = 
-    syst_diff_1.Invoke(static_cast<histtype(histtype::*)(const histtype&) const>(&histtype::operator/), nominal);
+  auto syst_div_1 = syst_diff_1.Invoke(static_cast<histtype(histtype::*)(const histtype&) const>(&histtype::operator/), nominal);
   TEST_SYSTEMATIC("one sided division", syst_div_1, (up - nominal) / nominal, (up - nominal) / nominal);
 
   TFile * output = new TFile("test_systematic.root", "update");
@@ -130,6 +128,7 @@ bool run_tests(bool verbose, std::string dir)
   return pass;
 }
 
+
 template<typename Scalar, int Cols>
 bool run_tests_mv(bool verbose, std::string dir)
 {
@@ -149,12 +148,12 @@ bool run_tests_mv(bool verbose, std::string dir)
     universes[i] = nominal + (-1 + (maxy - miny) / (nuniverses-1) * i);
   }
   
-  MultiverseSystematic syst("test_mv", universes);
+  Systematic syst("test_mv", universes);
 
   auto plus_1sigma = syst.NSigmaShift(1, nominal);
   auto minus_1sigma = syst.NSigmaShift(-1, nominal);
 
-  // MultiverseSystematic::NSigmaShift returns a 
+  // Systematic::NSigmaShift returns a 
   // histogram representing closest universes to 1 sigma away from nominal
   // this is done bin by bin, so the returned histogram holds values that are found
   // in the multiverse
@@ -167,43 +166,44 @@ bool run_tests_mv(bool verbose, std::string dir)
   int p1_idx = (0.5 - std::erf(1 / std::sqrt(2)) / 2.0) * (nuniverses-1) + 1;
   TEST_ARRAY("minus 1 sigma", minus_1sigma.Contents(), universes[p1_idx].Contents(), 0);
 
-  // Test the ability of MultiverseSystematic<T> to call T::ToHist
+  // Test the ability of Systematic<T> to call T::ToHist
   // No need to get too fancy here. If it compiles assume it passes
   std::vector<Ratio<Scalar, Cols> >vratio_mv;
-  for(auto i = 0u; i < syst.GetUniverses().size(); i++) {
-    vratio_mv.push_back(Ratio(syst.GetUniverses()[i], nominal));
+  for(auto i = 0u; i < syst.GetShifts().size(); i++) {
+    vratio_mv.push_back(Ratio(syst.GetShifts()[i], nominal));
   }
   
-  MultiverseSystematic ratio_mv("ratio_mv", vratio_mv);
-  MultiverseSystematic ratio_mv_hists = ratio_mv.Invoke(&Ratio<Scalar, Cols>::ToHist);
+  Systematic ratio_mv("ratio_mv", vratio_mv);
+  auto ratio_mv_hists = ratio_mv.Invoke(&Ratio<Scalar, Cols>::ToHist);
 
   auto ratio_plus_1sigma = ratio_mv.NSigmaShift(1, Ratio(nominal, nominal));
-  auto ratio_minus_1sigma = ratio_mv.NSigmaShift(-11, Ratio(nominal, nominal));
-
+  auto ratio_minus_1sigma = ratio_mv.NSigmaShift(-1, Ratio(nominal, nominal));
   
   // save everything for later inspection
   TFile * output = new TFile("test_systematic.root", "update");
   syst.SaveTo(output->mkdir(dir.c_str()), "test_mv");
-  ratio_mv_hists.SaveTo(output->GetDirectory(dir.c_str()), "ratio_mv");
   nominal.SaveTo(output->GetDirectory(dir.c_str()), "nominal");
+
+  ratio_mv_hists.SaveTo(output->GetDirectory(dir.c_str()), "ratio_mv");
   plus_1sigma.SaveTo(output->GetDirectory(dir.c_str()), "plus_1sigma");
   minus_1sigma.SaveTo(output->GetDirectory(dir.c_str()), "minus_1sigma");
   ratio_plus_1sigma.SaveTo(output->GetDirectory(dir.c_str()),  "ratio_plus_1sigma");
   ratio_minus_1sigma.SaveTo(output->GetDirectory(dir.c_str()), "ratio_minus_1sigma");
-
 
   output->Close();
   delete output;
 
   // serialization closure
   TFile * input = TFile::Open("test_systematic.root");
-  auto loaded = MultiverseSystematic<histtype>::LoadFrom(input->GetDirectory(dir.c_str()), "test_mv");
+  auto loaded = Systematic<histtype>::LoadFrom(input->GetDirectory(dir.c_str()), "test_mv");
   input->Close();
   delete input;
   TEST_MULTIVERSE("loadfrom/saveto", syst, *loaded);
 
   return pass;
 }
+
+
 
 int main(int argc, char ** argv)
 {
@@ -223,5 +223,63 @@ int main(int argc, char ** argv)
   pass &= run_tests_mv<double, Eigen::Dynamic>(verbose, "mv_double_dynamic");
   pass &= run_tests_mv<float, 10>(verbose, "mv_float_10");
   pass &= run_tests_mv<float, Eigen::Dynamic>(verbose, "mv_float_dynamic");
+
+  
+
+  // test of runtime
+  auto bins = Eigen::Array<double, 1, 11>::LinSpaced(11, 0, 10);
+  auto vnominal = Eigen::Array<double, 1, 10>::LinSpaced(10, -5, 5);
+  Hist<double, 10> nominal(vnominal, bins);
+  Ratio<double, 10> up(nominal + 1, nominal);
+  Ratio<double, 10> dw(nominal - 1, nominal);
+
+  int nuniverses = 50;
+  double maxy =  1;
+  double miny = -1;
+  std::vector<Ratio<double, 10> > universes(nuniverses);
+  for(auto i = 0; i < nuniverses; i++) {
+    universes[i] = Ratio(nominal + (-1 + (maxy - miny) / (nuniverses-1) * i),
+			 nominal);
+  }
+  
+  std::vector<Systematic<Ratio<double, 10> > * > systs;
+  systs.push_back(new Systematic<Ratio<double, 10> >("syst", up, dw));
+  systs.push_back(new Systematic<Ratio<double, 10> >("mv_syst", universes));
+
+  try {
+    systs[0]->NSigmaShift(0, Ratio(nominal, nominal));
+    pass &= false;
+  }
+  catch( exceptions::SystematicTypeError & e) {
+    if(verbose) std::cout << e.what() << std::endl;
+    pass &= true;
+  }
+  try {
+    systs[0]->Up();
+    pass &= true;
+  }
+  catch( exceptions::SystematicTypeError & e) {
+    if(verbose) std::cout << e.what() << std::endl;
+    pass &= false;
+  }
+
+
+  try {
+    systs[1]->NSigmaShift(0, Ratio(nominal, nominal));
+    pass &= true;
+  }
+  catch( exceptions::SystematicTypeError & e) {
+    if(verbose) std::cout << e.what() << std::endl;
+    pass &= false;
+  }
+  try {
+    systs[1]->Up();
+    pass &= false;
+  }
+  catch( exceptions::SystematicTypeError & e) {
+    if(verbose) std::cout << e.what() << std::endl;
+    pass &= true;
+  }
+
   if(pass) std::cout << "Success!" << std::endl;
 }
