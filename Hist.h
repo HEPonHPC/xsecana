496630c3905e6c588a8ffd9321e9d25a3d6d63e6
dddoyle1 <dddoyle@colostate.edu>
2021-08-25 12:04:59 -0500

    Automatic exposure scaling

--- Hist.h
+++ Hist.h
@@ -28,8 +28,14 @@ namespace xsec {
     Hist() {}
     Hist(const Eigen::Array<Scalar, 1, Cols> & contents,
 	 const Eigen::Array<Scalar, 1, EdgesSize(Cols)> & edges)
-      : fContents(contents), fEdges(edges)
+      : fContents(contents), fEdges(edges), fExposure(1)
     {}
+    Hist(const Eigen::Array<Scalar, 1, Cols> & contents,
+	 const Eigen::Array<Scalar, 1, EdgesSize(Cols)> & edges,
+	 Scalar exposure)
+      : fContents(contents), fEdges(edges), fExposure(exposure)
+    {}
+    
 
     // convenience constructor.
     // works for dynamic and fixed-size histograms
@@ -62,19 +68,24 @@ namespace xsec {
     Hist operator/=(const Scalar& rhs);
     Hist operator*=(const Scalar& rhs);    
 
-    // convenience wrappers of InvokeEigen
+    Hist ScaleByExposure(Scalar new_expo) const;
+    Hist TrueDivide(const Hist& rhs) const;
+
+    // some convenience functions
     Hist abs()  const;
     Hist abs2() const;
     Hist sqrt() const;
+    Hist pow(Scalar exp) const;
 
     const Eigen::Array<Scalar, 1, Cols  > & Contents() const { return fContents; }
-    const Eigen::Array<Scalar, 1, EdgesSize(Cols)> & Edges()    const { return fEdges   ; }
+    const Eigen::Array<Scalar, 1, EdgesSize(Cols)> & Edges() const { return fEdges; }
+    Scalar Exposure() const { return fExposure; }
 
-    unsigned int NBins() const { return fContents.size(); }
+    unsigned int size() const { return fContents.size(); }
     Eigen::Array<Scalar, 1, Cols> BinWidths() const;
-
-    Scalar GetBinContent(int i) const { return fContents(i); }
-    void SetBinContent(int i, Scalar val) { fContents(i) = val; }
+    
+    Scalar & operator[](int index) { return fContents(index); }
+    Scalar operator[](int index) const { return fContents(index); }
 
     void SaveTo(TDirectory * dir, std::string subdir) const;
     static std::unique_ptr<Hist> LoadFrom(TDirectory * dir, std::string subdir);
@@ -89,10 +100,11 @@ namespace xsec {
 
     Eigen::Array<Scalar, 1, Cols     > fContents;
     Eigen::Array<Scalar, 1, EdgesSize(Cols)> fEdges;
+    Scalar fExposure=1;
   };
 
   typedef Hist<double, Eigen::Dynamic> HistXd;
-  typedef Hist<float , Eigen::Dynamic> HistXf;
+  typedef Hist<float , Eigen::Dynamic> HistXf;  
 
   // ROOT interface
   // we're still dependent enough on ROOT for this to be here
@@ -105,17 +117,17 @@ namespace xsec {
     {
       TH1 * h;
       if constexpr(std::is_same<Scalar, double>::value) {
-	  h = new TH1D(name.c_str(), title.c_str(), hist.NBins(), hist.Edges().data());
+	  h = new TH1D(name.c_str(), title.c_str(), hist.size(), hist.Edges().data());
 	}
       else if constexpr(std::is_same<Scalar, float>::value) {
-	  h = new TH1F(name.c_str(), title.c_str(), hist.NBins(), hist.Edges().data());
+	  h = new TH1F(name.c_str(), title.c_str(), hist.size(), hist.Edges().data());
 	}
       else {
 	std::cerr << "ToTH1 not implemented for this type" << std::endl;
 	exit(1);
       }
 
-      for(auto i = 0u; i < hist.NBins(); i++) {
+      for(auto i = 0u; i < hist.size(); i++) {
 	h->SetBinContent(i+1, hist.Contents()(i));
       }
 
@@ -124,7 +136,7 @@ namespace xsec {
 
     template<typename Scalar, int Cols>
     Hist<Scalar, Cols> 
-    FromTH1(const TH1 * h)
+    FromTH1(const TH1 * h, Scalar expo)
     {      
       Scalar edges   [h->GetNbinsX()+1];
       Scalar contents[h->GetNbinsX()  ];
@@ -137,8 +149,30 @@ namespace xsec {
       // TODO this won't work for dynamic-sized arrays
       // Probaly lots of work needs to be done to handle this case.
       return Hist<Scalar, Cols>(Eigen::Map<Eigen::Array<Scalar, 1, Cols     > >(contents, h->GetNbinsX()  ),
-				Eigen::Map<Eigen::Array<Scalar, 1, EdgesSize(Cols)> >(edges   , h->GetNbinsX()+1));
+				Eigen::Map<Eigen::Array<Scalar, 1, EdgesSize(Cols)> >(edges   , h->GetNbinsX()+1),
+				expo);
     }
+  }
+  /////////////////////////////////////////////////////////
+  template<typename Scalar, int Cols>
+  Hist<Scalar, Cols> 
+  Hist<Scalar, Cols>::
+  TrueDivide(const Hist & rhs) const
+  {
+    return Hist<Scalar, Cols>(fContents / rhs.Contents(),
+			      fEdges,
+			      fExposure);
+  }
+
+  /////////////////////////////////////////////////////////
+  template<typename Scalar, int Cols>
+  Hist<Scalar, Cols> 
+  Hist<Scalar, Cols>::
+  ScaleByExposure(Scalar new_expo) const
+  {
+    return Hist<Scalar, Cols>(fContents * (new_expo / fExposure),
+			      fEdges,
+			      new_expo);
   }
 
   /////////////////////////////////////////////////////////
@@ -174,7 +208,17 @@ namespace xsec {
     dir->cd();
 
     auto h = root::ToTH1(*this);
-    h->Write("th1");
+    h->Write("hist");
+
+    TH1 * exposure;
+    if constexpr(std::is_same<Scalar, double>::value) {
+	exposure = new TH1D("", "", 1, 0, 1);
+      }
+    else if constexpr(std::is_same<Scalar, float>::value) {
+	exposure = new TH1F("", "", 1, 0, 1);
+      }
+    exposure->SetBinContent(1, fExposure);
+    exposure->Write("exposure");
 
     tmp->cd();
   }
@@ -190,11 +234,14 @@ namespace xsec {
     dir->cd();
 
     TH1 * h;
+    TH1 * exposure;
     if constexpr(std::is_same<Scalar, double>::value) {
-	h = (TH1D*) dir->Get("th1");
+	h = (TH1D*) dir->Get("hist");
+	exposure = (TH1D*) dir->Get("exposure");
       }
     else if constexpr(std::is_same<Scalar, float>::value) {
-	h = (TH1F*) dir->Get("th1");
+	h = (TH1F*) dir->Get("hist");
+	exposure = (TH1F*) dir->Get("exposure");
       }
     else {
       std::cerr << "Hist::LoadFrom not implemented for this type" << std::endl;
@@ -206,7 +253,7 @@ namespace xsec {
       exit(1);
     }
       
-    return std::make_unique<Hist<Scalar, Cols> > (root::FromTH1<Scalar, Cols>(h));
+    return std::make_unique<Hist<Scalar, Cols> > (root::FromTH1<Scalar, Cols>(h, exposure->GetBinContent(1)));
   }
 
   /////////////////////////////////////////////////////////
@@ -218,7 +265,8 @@ namespace xsec {
   {
     return 
       (this->fContents - rhs.fContents).isZero(0) && 
-      (this->fEdges - rhs.fEdges).isZero(0);
+      (this->fEdges - rhs.fEdges).isZero(0),
+      (this->fExposure == rhs.fExposure);
   }
 
   /////////////////////////////////////////////////////////
@@ -293,6 +341,17 @@ namespace xsec {
 	   int Cols>
   Hist<Scalar, Cols>
   Hist<Scalar, Cols>::
+  pow(Scalar exp) const
+  {
+    return Hist<Scalar, Cols>(fContents.pow(exp),
+			      fEdges);
+  }
+
+  /////////////////////////////////////////////////////////
+  template<typename Scalar, 
+	   int Cols>
+  Hist<Scalar, Cols>
+  Hist<Scalar, Cols>::
   operator-(const Hist & rhs) const
   {
     Hist<Scalar, Cols> ret = *this; // copy this
@@ -307,7 +366,7 @@ namespace xsec {
   Hist<Scalar, Cols>::
   operator-=(const Hist & rhs)
   {
-    this->fContents -= rhs.fContents;
+    this->fContents -= rhs.ScaleByExposure(fExposure).fContents;
     return *this;
   }
 
@@ -330,7 +389,7 @@ namespace xsec {
   Hist<Scalar, Cols>::
   operator+=(const Hist & rhs)
   {
-    this->fContents += rhs.fContents;
+    this->fContents += rhs.ScaleByExposure(fExposure).fContents;
     return *this;
   }
 
@@ -353,7 +412,7 @@ namespace xsec {
   Hist<Scalar, Cols>::
   operator*=(const Hist & rhs)
   {
-    this->fContents *= rhs.fContents;
+    this->fContents *= rhs.ScaleByExposure(fExposure).fContents;
     return *this;
   }
 
@@ -376,7 +435,7 @@ namespace xsec {
   Hist<Scalar, Cols>::
   operator/=(const Hist & rhs)
   {
-    this->fContents /= rhs.fContents;
+    this->fContents /= rhs.ScaleByExposure(fExposure).fContents;
     return *this;
   }
 
