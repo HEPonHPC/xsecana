ccc3e36387516076ae63b88ce427a97e510f8a22
dddoyle1 <dddoyle@colostate.edu>
2021-08-31 17:17:44 -0500

    Interface for the measurement that the analysis object handles

---
+++ include/XSecAna/IMeasurement.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "TDirectory.h"
+#include "Hist.h"
+
+namespace xsec {
+  template<class HistType>
+  class IMeasurement {
+  public:
+    virtual HistType Result(const HistType & data) = 0;
+
+    virtual void SaveTo(TDirectory * dir, const std::string& subdir) const = 0;
+
+    /// \brief Children must override this function
+    static std::unique_ptr<IMeasurement> LoadFrom(TDirectory * dir, const std::string& name)
+    {
+      assert(false && "IMeasurement::LoadFrom not implemented");
+    }
+  private:
+
+  };
+}
