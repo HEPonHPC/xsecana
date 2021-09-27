2a62780b6011e32b56ee1013107b05c6ab9c034f
Derek Doyle <derek.doyle@colostate.edu>
2021-09-27 15:23:35 -0600

    Classic couts for debugging an issue

--- test/test_template_fit_calculator.cc
+++ test/test_template_fit_calculator.cc
@@ -7,6 +7,7 @@
 
 #include <Eigen/Dense>
 #include <iostream>
+#include <iomanip>
 
 #include "TFile.h"
 
@@ -67,6 +68,12 @@ int main(int argc, char ** argv) {
     assert((reduced_user_params - param_map.ToUserParams(reduced_minimizer_params)).isZero(0));
     assert((reduced_minimizer_params - param_map.ToMinimizerParams(user_params)).isZero(0));
 
+    std::vector<double> std_vector;
+    for(auto i = 0u; i < 10; i++) std_vector.push_back(i);
+    Eigen::VectorXd from_std = fit::detail::STDToEigen(std_vector);
+    for(auto i = 0u; i < std_vector.size(); i++) {
+        assert(from_std[i] == std_vector[i]);
+    }
 
     std::vector<int> dims = {4, 10};
     Eigen::Matrix<double, -1, -1, Eigen::RowMajor> signal_templates(dims[0], dims[1]);
@@ -99,28 +106,67 @@ int main(int argc, char ** argv) {
 
     assert((total - fit_calc->Predict(user_params)).isZero(0));
 
-    user_params(user_params.size() - 1) = 4;
-    fit_calc->FixTemplate(fit_calc->GetNTemplates() - 1, 4);
-    assert((fit_calc->U(minimizer_params) - fit_calc->Predict(user_params)).isZero());
-
-    // make sure we can fix the same template to a different value
-    user_params(user_params.size() - 1) = 5;
-    fit_calc->FixTemplate(fit_calc->GetNTemplates() - 1, 5);
-    assert((fit_calc->U(minimizer_params) - fit_calc->Predict(user_params)).isZero());
+    // TODO make sure we can fix the same template to a different value
 
     // now release a template
     fit_calc->ReleaseTemplate(fit_calc->GetNTemplates() - 1);
     user_params = Eigen::RowVectorXd::Ones(templates.size() * dims[0]);
     assert((total - fit_calc->U(user_params)).isZero(0));
 
-
     user_params(4) = 1.5;
 
     fit::Minuit2TemplateFitter fitter(fit_calc, 3);
-    fitter.SetPrintLevel(1);
+    fitter.SetPrintLevel(0);
     auto result = fitter.Fit(fit_calc->Predict(user_params));
 
-    std::cout << result.params << std::endl;
+    user_params(4) = 1;
+
+    std::vector<double> p;
+    std::vector<double> c;
+    std::vector<double> c2;
+    std::vector<double> c3;
+    std::vector<double> std_params(user_params.size()-1);
+    auto eigen_params = user_params;
+    double nfits = 5;
+    for(auto i = 0u; i < nfits; i++) {
+        auto delta = 0. + (20 / nfits) * i;
+        fit_calc->FixTemplate(user_params.size()-1, delta);
+        auto r = fitter.Fit(total);
+        p.push_back(delta);
+        c.push_back(r.fun_val);
+
+        for(auto i = 0u; i < std_params.size()-1; i++) {
+            std_params[i] = r.params(i);
+
+        }
+        std::cout << r.params.transpose() << std::endl;
+        std_params[std_params.size()-1] = delta;
+
+        c2.push_back(fit_calc->fun(fit::detail::STDToEigen(std_params), total));
+        c3.push_back(fitter(std_params));
+    }
+
+    for(auto i = 0u; i < nfits; i++) {
+        std::cout << std::setw(20) << p[i] << "|";
+    }
+    std::cout << std::endl;
+    for(auto i = 0u; i < nfits; i++) {
+        std::cout << std::setw(20) << c[i] << "|";
+    }
+    std::cout << std::endl;
+    for(auto i = 0u; i < nfits; i++) {
+        std::cout << std::setw(20) << c2[i] << "|";
+    }
+    std::cout << std::endl;
+    for(auto i = 0u; i < nfits; i++) {
+        std::cout << std::setw(20) << c3[i] << "|";
+    }
+    std::cout << std::endl;
+
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << result.params.transpose() << std::endl;
+    std::cout << result.plus_one_sigma_errors.transpose() << std::endl;
+    std::cout << result.minus_one_sigma_errors.transpose() << std::endl;
     std::cout << result.fun_val << std::endl;
     std::cout << result.fun_calls << std::endl;
 }
