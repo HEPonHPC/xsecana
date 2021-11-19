163808840d1f05b0ad95579596905f1839230fba
Derek Doyle <derek.doyle@colostate.edu>
2021-11-19 11:16:50 -0700

    Random seed functions

--- include/XSecAna/Fit/IFitter.h
+++ include/XSecAna/Fit/IFitter.h
@@ -4,6 +4,8 @@
 #pragma once
 #include <Eigen/Dense>
 #include "XSecAna/IMeasurement.h"
+#include <iomanip>
+#include <iostream>
 
 namespace xsec {
     namespace fit {
@@ -30,6 +32,50 @@ namespace xsec {
             virtual unsigned int GetNUserParams() const = 0;
             virtual unsigned int GetNFunCalls() const = 0;
             virtual Vector ToUserParams(const Vector & minimizer_coords) const = 0;
+            virtual Vector ToMinimizerParams(const Vector & user_coords) const = 0;
+
+            std::vector<Array> GetRandomSeeds(int n, double lb, double ub) const {
+                std::default_random_engine generator;
+                std::uniform_real_distribution<double> distribution(lb, ub);
+
+                std::vector<Array> seeds(n);
+                for(auto ithrow = 0u; ithrow < n; ithrow++) {
+                    seeds[ithrow] = Array::Ones(this->GetNMinimizerParams());
+                    for (auto iparam = 0u; iparam < seeds[ithrow].size(); iparam++) {
+                        seeds[ithrow](iparam) += distribution(generator);
+                    }
+                }
+                return seeds;
+            }
+
+            std::vector<Vector> GetBestSeeds(const Array & data,
+                                             const std::vector<Array> & seeds,
+                                             int take) const {
+                std::vector<double> chi2(seeds.size());
+                std::vector<int> seed_idx(seeds.size());
+                for(auto i = 0u; i < seeds.size(); i++) {
+                    seed_idx[i] = i;
+                    chi2[i] = this->fun(seeds[i], data);
+                }
+
+                // sort seed_idx based on ascending chi2 values
+                std::sort(seed_idx.begin(), seed_idx.end(),
+                          [&chi2](int a, int b) -> bool {
+                              return chi2[a] < chi2[b];
+                          });
+
+                std::cout << "Seeding fit with the following parameter configurations:" << std::endl;
+                std::cout << "--------------------------------------------------------" << std::endl;
+                std::cout << std::left;
+                std::cout << std::setw(20) << "chi-squared" << "|" << " seed" << std::endl;
+                std::cout << "--------------------------------------------------------" << std::endl;
+                std::vector<Vector> best_seeds(take);
+                for (auto iseed = 0u; iseed < take; iseed++) {
+                    best_seeds[iseed] = seeds[seed_idx[iseed]];
+                    std::cout << std::setw(20) << chi2[seed_idx[iseed]] << "| " << best_seeds[iseed].transpose() << std::endl;
+                }
+                return best_seeds;
+            }
         };
 
         class IFitter {
